eedde35e8dcc9aafe987f6f5e17db91c7d6fffde
stanislavshvets <stanislavshvets.com@gmail.com>
2025-09-18 21:00:46 +0300

    add test-migration

--- DjangoPart/CustomerStats/migrations/0006_deliverytype_alter_client_delivery_type_and_more.py
+++ DjangoPart/CustomerStats/migrations/0006_deliverytype_alter_client_delivery_type_and_more.py
@@ -1,71 +1,92 @@
 import django
 from django.db import migrations, models
 
-def forwards(apps, schema_editor):
+def create_delivery_types(apps, schema_editor):
+    DeliveryType = apps.get_model("CustomerStats", "DeliveryType")
+
+    # створюємо записи
+    for dtype in ["post", "taxi", "courier"]:
+        DeliveryType.objects.create(delivery_type=dtype)
+
+
+def migrate_client_delivery(apps, schema_editor):
     Client = apps.get_model("CustomerStats", "Client")
     Order = apps.get_model("CustomerStats", "Order")
     DeliveryType = apps.get_model("CustomerStats", "DeliveryType")
 
-    # Створюємо унікальні типи доставки
-    delivery_map = {}
-    for dt in ["post", "taxi", "courier"]:
-        obj, _ = DeliveryType.objects.get_or_create(delivery_type=dt)
-        delivery_map[dt] = obj
+    # робимо мапу { "post": <DeliveryType об'єкт>, ... }
+    mapping = {dt.delivery_type: dt for dt in DeliveryType.objects.all()}
 
-    # Оновлюємо клієнтів
+    # переносимо дані для Client
     for client in Client.objects.all():
-        if client.delivery_type:
-            client.delivery_type = delivery_map.get(client.delivery_type)
-            client.save()
+        if client.delivery_type:  # старе текстове поле
+            client.delivery_type_fk = mapping[client.delivery_type]
+            client.save(update_fields=["delivery_type_fk"])
 
-    # Оновлюємо замовлення
+    # переносимо дані для Order
     for order in Order.objects.all():
-        if order.delivery_type:
-            order.delivery_type = delivery_map.get(order.delivery_type)
-            order.save()
-
-
-def backwards(apps, schema_editor):
-    Client = apps.get_model("CustomerStats", "Client")
-    Order = apps.get_model("CustomerStats", "Order")
-
-    # Повертаємо назад у CharField (рядки)
-    for client in Client.objects.all():
-        if client.delivery_type:
-            client.delivery_type = client.delivery_type.delivery_type
-            client.save()
-
-    for order in Order.objects.all():
-        if order.delivery_type:
-            order.delivery_type = order.delivery_type.delivery_type
-            order.save()
+        if order.delivery_type:  # старе текстове поле
+            order.delivery_type_fk = mapping[order.delivery_type]
+            order.save(update_fields=["delivery_type_fk"])
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("CustomerStats", "0005_seed_data"),  # заміни на свою першу міграцію
+        ("CustomerStats", "0005_seed_data"),
     ]
 
     operations = [
+        # 1. створюємо нову таблицю
         migrations.CreateModel(
-            name='DeliveryType',
+            name="DeliveryType",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('delivery_type', models.CharField(max_length=100)),
+                ("id", models.AutoField(primary_key=True, serialize=False)),
+                ("delivery_type", models.CharField(max_length=100)),
             ],
         ),
-        migrations.AlterField(
-            model_name='client',
-            name='delivery_type',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.PROTECT,
-                                    to='CustomerStats.deliverytype'),
+
+        # 2. додаємо тимчасові поля з ForeignKey (null=True)
+        migrations.AddField(
+            model_name="client",
+            name="delivery_type_fk",
+            field=models.ForeignKey(
+                to="CustomerStats.DeliveryType",
+                on_delete=models.PROTECT,
+                null=True,
+                related_name="+",
+            ),
         ),
-        migrations.AlterField(
-            model_name='order',
-            name='delivery_type',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.PROTECT,
-                                    to='CustomerStats.deliverytype'),
+        migrations.AddField(
+            model_name="order",
+            name="delivery_type_fk",
+            field=models.ForeignKey(
+                to="CustomerStats.DeliveryType",
+                on_delete=models.PROTECT,
+                null=True,
+                related_name="+",
+            ),
         ),
-        migrations.RunPython(forwards, backwards),
+
+        # 3. створюємо записи у DeliveryType
+        migrations.RunPython(create_delivery_types),
+
+        # 4. переносимо дані
+        migrations.RunPython(migrate_client_delivery),
+
+        # 5. видаляємо старі поля
+        migrations.RemoveField(model_name="client", name="delivery_type"),
+        migrations.RemoveField(model_name="order", name="delivery_type"),
+
+        # 6. перейменовуємо нові поля на правильне ім’я
+        migrations.RenameField(
+            model_name="client",
+            old_name="delivery_type_fk",
+            new_name="delivery_type",
+        ),
+        migrations.RenameField(
+            model_name="order",
+            old_name="delivery_type_fk",
+            new_name="delivery_type",
+        ),
     ]
--- DjangoPart/CustomerStats/views.py
+++ DjangoPart/CustomerStats/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import render
 
 from .models import Client, Order
 
+def get_client_orders(client_id: int):
+    client = Client.objects.get(pk=client_id)
+    orders = Order.objects.filter(client=client).select_related("delivery_type")
+
+    return {
+        "Client.name": client.name,
+        "Order": [
+            {
+                "delivery_type": order.delivery_type.delivery_type if order.delivery_type else None,
+                "name": order.name,
+            }
+            for order in orders
+        ]
+    }
 
 # Create your views here.
 def index(request):
